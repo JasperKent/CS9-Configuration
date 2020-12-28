1c938278df257cafffc8a67dce0f90312a8de817
Jasper Kent <jasper_kent@hotmail.com>
2020-12-28 10:01:47 +0000

    Add project files.

---
+++ CS9Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CS9Demo
+{
+    class SomeClass
+    {
+        public string Name { get; init; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CS9Library/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CS9Library
+{
+    public class Class1
+    {
+        public string Name { get; init; }
+    }
+}
